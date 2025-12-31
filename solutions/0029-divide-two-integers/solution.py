c4d5f1ec5e9157e96a4f757fae4b4e250557d0a3
Tharun R <tharunritz37@gmail.com>
2025-12-31 18:16:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (79.33%)

---
+++ solutions/0029-divide-two-integers/solution.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=29 lang=python3
+#
+# [29] Divide Two Integers
+#
+
+# @lc code=start
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        if divisor == 0:
+            return INT_MAX
+        if dividend == INT_MIN and divisor == -1:
+            return INT_MAX
+        negative = (dividend < 0) ^ (divisor < 0)
+        a, b = abs(dividend), abs(divisor)
+        result = 0
+        while a >= b:
+            temp = b
+            multiple = 1
+            while a >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+            a -= temp
+            result += multiple
+
+        if negative:
+            result = -result
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+        return result
+
+
+
