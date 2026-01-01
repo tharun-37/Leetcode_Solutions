907e2d8f200420c4dcb72fa3fd78d82dfc4f8264
Tharun R <tharunritz37@gmail.com>
2026-01-01 11:56:44 +0000

    Sync LeetCode submission Runtime - 10 ms (72.51%), Memory - 19.7 MB (6.80%)

---
+++ solutions/0042-trapping-rain-water/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        count = 0
+        
+        for i, h in enumerate(height):
+            while stack and h > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                distance = i - stack[-1] - 1
+                bounded_height = min(h, height[stack[-1]]) - height[top]
+                count += distance * bounded_height
+            stack.append(i)
+            
+        return count
+                        
+                    
