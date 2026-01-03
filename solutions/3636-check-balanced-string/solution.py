9104763c84ecfad28611edd8ed157afd55b48ac2
Tharun R <tharunritz37@gmail.com>
2026-01-03 10:35:27 +0000

    Sync LeetCode submission Runtime - 3 ms (56.18%), Memory - 17.2 MB (98.52%)

---
+++ solutions/3636-check-balanced-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        even, odd = 0, 0
+        for i, val in enumerate(num):
+            if i % 2 == 0:
+                even += int(val)
+            else:
+                odd += int(val)
+        return even == odd
