914cb18aa6b8026aa7d0977a2e568dbede02a103
Tharun R <tharunritz37@gmail.com>
2026-01-06 16:46:24 +0000

    Sync LeetCode submission Runtime - 88 ms (92.17%), Memory - 51.3 MB (38.02%)

---
+++ solutions/0204-count-primes/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                count++;
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        notPrime[j] = true;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+}
