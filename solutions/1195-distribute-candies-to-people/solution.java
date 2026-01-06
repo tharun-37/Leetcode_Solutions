5d996109a8db1ffd7d4617e85b30c9d1e46f1776
Tharun R <tharunritz37@gmail.com>
2026-01-06 16:39:10 +0000

    Sync LeetCode submission Runtime - 3 ms (5.95%), Memory - 42.7 MB (20.48%)

---
+++ solutions/1195-distribute-candies-to-people/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int arr[] = new int[num_people];
+        int rot = 0;
+        while (candies > 0) {
+            for (int i = 0; i < num_people; i++) {
+                if (candies <= (rot * num_people) + (i + 1)) {
+                    arr[i] += candies;
+                    candies = 0;
+                    break;
+                }
+                arr[i] += (rot * num_people) + (i + 1);
+                candies -= (rot * num_people) + (i + 1);
+            }
+            rot++;
+        }
+        return arr;
+    }
+}
