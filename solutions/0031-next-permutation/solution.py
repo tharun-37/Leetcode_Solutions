3720dd8b5ff704502407b86345f40389f604925b
Tharun R <tharunritz37@gmail.com>
2026-01-07 17:55:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (9.82%)

---
+++ solutions/0031-next-permutation/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+  def nextPermutation(self, nums: list[int]) -> None:
+    n = len(nums)
+    i = n - 2
+    while i >= 0:
+      if nums[i] < nums[i + 1]:
+        break
+      i -= 1
+    if i >= 0:
+      for j in range(n - 1, i, -1):
+        if nums[j] > nums[i]:
+          nums[i], nums[j] = nums[j], nums[i]
+          break
+    def reverse(nums: list[int], l: int, r: int) -> None:
+      while l < r:
+        nums[l], nums[r] = nums[r], nums[l]
+        l += 1
+        r -= 1
+    reverse(nums, i + 1, len(nums) - 1)
