c215644b3b4a7b877a855da9cef5dc58c680ff24
Tharun R <tharunritz37@gmail.com>
2026-01-13 04:23:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.8 MB (72.91%)

---
+++ solutions/0050-powx-n/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N == 0) return 1.0;
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+        double res = 1.0;
+        while (N > 0) {
+            if ((N & 1) == 1) res *= x;
+            x *= x;
+            N >>= 1;
+        }
+        return res;
+    }
+}
