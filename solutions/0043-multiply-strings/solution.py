eff31c269c2f3adf58fd98d012c646b0f750362d
Tharun R <tharunritz37@gmail.com>
2026-01-13 04:05:19 +0000

    Sync LeetCode submission Runtime - 1 ms (66.80%), Memory - 19.2 MB (10.93%)

---
+++ solutions/0043-multiply-strings/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        val1 = 0
+        for digit in num1:
+            val1 = val1 * 10 + (ord(digit) - ord('0'))
+            
+        val2 = 0
+        for digit in num2:
+            val2 = val2 * 10 + (ord(digit) - ord('0'))
+            
+        return str(val1 * val2)
