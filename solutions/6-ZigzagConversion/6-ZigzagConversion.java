738adbacf814ff2811314b94c32cd394a901daf9
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:18:35 +0530

    Create 6-ZigzagConversion

---
+++ solutions/6-ZigzagConversion/6-ZigzagConversion.java
@@ -0,0 +1,37 @@
+// Last updated: 5/16/2026, 10:18:38 PM
+class Solution {
+    public String convert(String s, int nr) {
+        if (nr == 1 || s.length() <= nr) {
+            return s;
+        }
+
+        StringBuilder[] rows = new StringBuilder[nr];
+
+        for (int i = 0; i < nr; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int in = 0;
+        int st = 1;
+
+        for (char c : s.toCharArray()) {
+            rows[in].append(c);
+
+            if (in == 0) {
+                st = 1;
+            } else if (in == nr - 1) {
+                st = -1;
+            }
+
+            in += st;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder sb : rows) {
+            result.append(sb);
+        }
+
+        return result.toString();
+    }
+}
