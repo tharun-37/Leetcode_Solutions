f4a968c09120462c3c63d10142f89516429d066d
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:16:52 +0530

    Create 287-FindTheDuplicateNumber

---
+++ solutions/287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 5/16/2026, 10:16:55 PM
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int first = nums[0];
+        int second = nums[0];
+        do {
+            first = nums[first];
+            second = nums[nums[second]];
+        } while (first != second);
+        first = nums[0];
+        while (first != second) {
+            first = nums[first];
+            second = nums[second];
+        }
+        return second;
+    }
+}
