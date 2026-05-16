0411a8d32a27c254fc8ae81f2f78695024f933ff
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:17:11 +0530

    Create 202-HappyNumber

---
+++ solutions/202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 5/16/2026, 10:17:13 PM
+class Solution {
+    public boolean isHappy(int n) {
+        int[] list = new int[100];
+        int j = 0;
+        while (n != 1) {
+            int sum = 0;
+            while (n > 0) {
+                sum += (n%10)*(n%10);
+                n /= 10;
+            }
+            for (int i = 0; i < j; i++) {
+                if (list[i] == sum) return false;
+            }
+            list[j++] = sum;
+            n = sum;
+        }
+        return true;
+    }
+}
