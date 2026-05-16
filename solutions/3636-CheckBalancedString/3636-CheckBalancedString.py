522a34d71f041289eb4fc19b7ac68c65cd6a5d84
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:16:16 +0530

    Create 3636-CheckBalancedString

---
+++ solutions/3636-CheckBalancedString/3636-CheckBalancedString.py
@@ -0,0 +1,10 @@
+# Last updated: 5/16/2026, 10:16:19 PM
+class Solution:
+    def isBalanced(self, num: str) -> bool:
+        even, odd = 0, 0
+        for i, val in enumerate(num):
+            if i % 2 == 0:
+                even += int(val)
+            else:
+                odd += int(val)
+        return even == odd
