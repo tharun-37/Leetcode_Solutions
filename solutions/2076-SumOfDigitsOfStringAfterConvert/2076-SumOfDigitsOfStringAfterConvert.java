60456a5d0a2c10dce973956905cce4885a72304d
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:16:26 +0530

    Create 2076-SumOfDigitsOfStringAfterConvert

---
+++ solutions/2076-SumOfDigitsOfStringAfterConvert/2076-SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,19 @@
+// Last updated: 5/16/2026, 10:16:28 PM
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            sb.append(c - 'a' + 1);
+        }
+        String currentString = sb.toString();
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum = 0;
+            for (char digitChar : currentString.toCharArray()) {
+                sum += digitChar - '0';
+            }
+            currentString = String.valueOf(sum);
+        }
+        return sum;
+    }
+}
