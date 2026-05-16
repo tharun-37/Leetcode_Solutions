66dac57f3412ee7c1b6420c7233fb0daf0142074
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:18:21 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ solutions/17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,34 @@
+// Last updated: 5/16/2026, 10:18:23 PM
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private List<String> result = new ArrayList<>();
+    private final String[] MAPPING = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private String digits;
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+        this.digits = digits;
+        findCombinations(0, new StringBuilder());
+        return result;
+    }
+
+    private void findCombinations(int index, StringBuilder currentCombination) {
+        if (index == digits.length()) {
+            result.add(currentCombination.toString());
+            return;
+        }
+
+        char digitChar = digits.charAt(index);
+        String letters = MAPPING[digitChar - '0'];
+
+        for (char letter : letters.toCharArray()) {
+            currentCombination.append(letter);
+            findCombinations(index + 1, currentCombination);
+            currentCombination.deleteCharAt(currentCombination.length() - 1);
+        }
+    }
+}
