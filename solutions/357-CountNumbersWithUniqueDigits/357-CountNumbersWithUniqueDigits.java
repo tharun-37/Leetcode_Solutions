59669ed27500a2179395a30474e0a5c49328074c
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:16:48 +0530

    Create 357-CountNumbersWithUniqueDigits

---
+++ solutions/357-CountNumbersWithUniqueDigits/357-CountNumbersWithUniqueDigits.java
@@ -0,0 +1,15 @@
+// Last updated: 5/16/2026, 10:16:50 PM
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+        int ans = 10;
+        int unique = 9;
+        int available = 9;
+        for (int i = 2; i <= n && i <= 10; i++) {
+            unique *= available;
+            ans += unique;
+            available--;
+        }
+        return ans;
+    }
+}
