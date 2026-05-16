c630dbf94eddb8944d9a356e97a022930d556c3c
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:17:53 +0530

    Create 58-LengthOfLastWord

---
+++ solutions/58-LengthOfLastWord/58-LengthOfLastWord.java
@@ -0,0 +1,10 @@
+// Last updated: 5/16/2026, 10:17:55 PM
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String st[]=s.split(" ");
+        int c=0;
+        for(int i=0;i<st[st.length-1].length();i++){
+            c++;
+        }return c;
+    }
+}
