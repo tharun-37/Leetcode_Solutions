6812a1a56140865eaf73f80558602fd37529c421
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:17:55 +0530

    Create 50-PowxN

---
+++ solutions/50-PowxN/50-PowxN.java
@@ -0,0 +1,18 @@
+// Last updated: 5/16/2026, 10:17:58 PM
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N == 0) return 1.0;
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+        double res = 1.0;
+        while (N > 0) {
+            if ((N & 1) == 1) res *= x;
+            x *= x;
+            N >>= 1;
+        }
+        return res;
+    }
+}
