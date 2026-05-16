46b762ab7b2bfe1712b9c6b3eb3d817ae3aa9a02
Tharun R <tharunritz37@gmail.com>
2026-05-16 22:16:49 +0530

    Create 344-ReverseString

---
+++ solutions/344-ReverseString/344-ReverseString.c
@@ -0,0 +1,14 @@
+// Last updated: 5/16/2026, 10:16:52 PM
+void reverseString(char* s, int sSize) {
+     int left = 0;
+    int right = sSize - 1;
+    while (left < right) {
+        // Swap characters at left and right pointers
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        // Move pointers towards the center
+        left++;
+        right--;
+    }
+}
